1c07c809e9ef636da70f61572ad1abbea854be6a
Songonha <105191398+songonha@users.noreply.github.com>
2022-12-10 22:32:29 +0700

    Update Websocket.js

--- client_side/Models/Websocket.js
+++ client_side/Models/Websocket.js
@@ -1,1 +1,28 @@
 
+new function (n) {
+    this.socket = '';
+    this.start = function (ws) {
+        this.socket = new WebSocket(ws);
+        this.socket.onopen = function (evt) {
+            console.log('connect to server');
+  
+        }
+        this.socket.onclose = function (data) {
+            console.log('Close to server');
+            $.notify("Mất kết nối tới máy chủ... Đang chờ kết nối lại.", "error");
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+        }
+        this.socket.onerror = function (data) {
+            console.log('khong the ket noi');
+            $.notify("Mất kết nối tới máy chủ... Đang chờ kết nối lại.", "error");
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+        }
+        
+    }
+  
+    n.dn = this;
+}(window, document);
