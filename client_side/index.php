e3278d25ca5e252143c8cd29380a2304997c237f
Songonha <105191398+songonha@users.noreply.github.com>
2022-12-10 22:27:48 +0700

    Update index.php

--- client_side/index.php
+++ client_side/index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>Admin - Control Admin</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+</head>
+<body></body>
 <?php
   
 ?>
+ <!-- Websocket -->
+<script src="Models/Websocket.js"></script>
+<!--  End -->
+</html>
